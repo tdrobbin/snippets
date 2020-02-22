aa1f69719d6992321f23a8b8c92b91c107a44246
tdrobbin <tylerdrobbin@gmail.com>
2020-02-22 18:59:41 -0500

    Update snippets2.py

--- snippets2.py
+++ snippets2.py
@@ -1,29 +1,3 @@
-import pandas as pd
-from pandas import *
-
-import scipy.stats as stats
-from scipy.stats import *
-
-pd.set_option('display.large_repr', 'info')
-pd.set_option('display.precision', 4)
-
-# %pylab inline
-ipython = get_ipython()
-ipython.run_line_magic('pylab', 'inline')
-ipython.run_line_magic('config', "InlineBackend.figure_format='retina'")
-
-from numpy import array, test, testing
-
-figsize(9,6)
-
-import seaborn as sns
-sns.set()
-
-from importlib import reload
-
-from dask.distributed import Client, Future
-
-
 def run_grid(func, client, **grid_kwargs):
     """
     eg:
