3806ac4651d12c03ce367675d28256813366201f
tdrobbin <tylerdrobbin@gmail.com>
2020-03-23 12:55:32 -0400

    Update jupyter_config.py

--- jupyter_config.py
+++ jupyter_config.py
@@ -20,14 +20,16 @@ import os
 
 _init_cwd = os.getcwd()
 
-# %pylab inline
+# %pylab inline + other magic
 ipython = get_ipython()
-ipython.run_line_magic('pylab', 'inline')
-ipython.run_line_magic('config', "InlineBackend.figure_format='retina'")
+ipython.magic('pylab inline')
+ipython.magic('config InlineBackend.figure_format="retina"')
+ipython.magic('load_ext autoreload')
+ipython.magic('autoreload 2')
 
 # cells can have multiple outputs
 from IPython.core.interactiveshell import InteractiveShell
-InteractiveShell.ast_node_interactivity = "all"
+InteractiveShell.ast_node_interactivity = "all"'axes'axes
 
 # df
 # set_option('display.large_repr', 'info')
@@ -41,6 +43,22 @@ plt.rcParams['axes.titlepad'] = 18
 plt.rcParams['legend.frameon'] = False
 # plt.rcParams['legend.borderaxespad'] = 0
 # plt.rcParams['legend.loc'] = (1.01, 0)
+
+# copied from quanstats styling:
+# https://github.com/ranaroussi/quantstats/blob/71fb349ddeb500ee65af3048c1bf9d481f20c415/quantstats/_plotting/core.py
+sns.set(font_scale=1.5, rc={
+    'figure.figsize': (9, 6),
+    'axes.facecolor': 'white',
+    'figure.facecolor': 'white',
+    'grid.color': '#dddddd',
+    'grid.linewidth': 0.5,
+    "lines.linewidth": 1.5,
+    'text.color': '#333333',
+    'xtick.color': '#666666',
+    'ytick.color': '#666666',
+    'axes.titlepad': 18,
+    'legend.frameon': False
+})
 
 # reset any bultin we may have overidden
 import builtins
